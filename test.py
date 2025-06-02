f693ad95b2c2e0a51b25b4d98591bf112ed2a708
Rajarsh <56415666+rajarshnaidu@users.noreply.github.com>
2025-06-01 20:55:12 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,10 @@ conf = sc._jvm.org.apache.spark.SparkEnv.get().conf()
 conf_map = {entry._1(): entry._2() for entry in conf.getAll()}
 for k, v in conf_map.items():
     print(k, "=>", v)
+
+
+jvm = sc._jvm
+loader = jvm.Thread.currentThread().getContextClassLoader()
+urls = loader.getURLs()
+for url in urls:
+    print(url.getPath())
